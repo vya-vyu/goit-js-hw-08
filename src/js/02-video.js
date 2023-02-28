48ec89ec815c689ed9ed91d054b2747e4b9e57be
vya-vyu <linda1307@gmail.com>
2023-02-28 19:23:39 +0000

    all tasks

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle'
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const TIME_PLAER_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onplay,1000));
+
+function onplay({seconds}) { 
+   localStorage.setItem(TIME_PLAER_KEY, JSON.stringify(seconds));
+}
+const actualTime = JSON.parse(localStorage.getItem(TIME_PLAER_KEY));
+player.setCurrentTime(actualTime);
+
+// player.setCurrentTime(actualTime).then(function(actualTime) {
+//     // seconds = the actual time that the player seeked to
+// }).catch(function(error) {
+//     switch (error.name) {
+//         case 'RangeError':
+//             // the time was less than 0 or greater than the video’s duration
+//             break;
+
+//         default:
+//             // some other error occurred
+//             break;
+//     }
+// });
+
+
+
+
+
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
 
+
+const form = document.querySelector('.feedback-form');
+const FEEDBACK_KEY = "feedback-form-state";
+const feedbackFromStorage = localStorage.getItem(FEEDBACK_KEY);
+console.log(feedbackFromStorage);
+
+if (feedbackFromStorage) { 
+    setDataFromLocalStorage(JSON.parse(feedbackFromStorage), form);
+} 
+
+form.addEventListener('submit',createSubmit);
+
+function createSubmit(event) { 
+    event.preventDefault();
+    
+    const { email, message } = event.currentTarget.elements;
+  
+    const objSubmit = {
+        email: email.value,
+        message:message.value
+    }
+
+    localStorage.setItem(FEEDBACK_KEY, JSON.stringify(objSubmit));
+}
+
+
+function setDataFromLocalStorage({email,message },parent) { 
+    parent.elements.email.value = email;
+    parent.elements.message.value = message;
+}
+
